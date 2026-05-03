5e43ce8cf7e64b49e007125e1cfa4ee682036313
Shashikanth1997 <shashikanthdussa08@gmail.com>
2026-05-03 15:13:50 +0530

    changes

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const db = require('./db');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 // GET all users
 app.get('/users', async (req, res) => {
   try {
